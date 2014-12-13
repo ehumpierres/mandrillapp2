3da146d135fced54a21b01248fec3f07c7f92f0b
Jhon Jairo Roa <jhonjairoroa87@gmail.com>
2014-12-13 18:19:26 -0500

    added validated user service

--- init.py
+++ init.py
@@ -19,7 +19,8 @@ from services.useractions.getshortlist import get_shortlist_api
 from services.useractions.emailconcierge import concierge_email_api
 from services.useractions.addusercomment import add_user_comment_api
 from services.useractions.createuser import create_user_api
-
+from services.useractions.getuserlists import get_user_lists_api
+from services.useractions.validateuser import validate_user_api
 
 #trotter actions
 from services.trotteractions.recommend import recommend_api
@@ -66,6 +67,8 @@ app.register_blueprint(add_user_comment_api)
 app.register_blueprint(get_comments_api)
 app.register_blueprint(get_listing_details_api)
 app.register_blueprint(create_user_api)
+app.register_blueprint(get_user_lists_api)
+app.register_blueprint(validate_user_api)
 
 
 @app.route('/')
