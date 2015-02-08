0a73b9f6bbb9a56e0b867979085244ffeb05d9a4
ehumpierres <ehumpierres@Ernestos-MacBook-Air-2.local>
2015-02-07 21:04:46 -0500

    to codeship

--- services/dashboard/dashboard.py
+++ services/dashboard/dashboard.py
@@ -136,7 +136,7 @@ def get_unread_notifications(user_id = None):
         print traceback.format_exc()
         abort(500)
 
-@dashboard_api.route('/mandrillreplies', methods=['POST']) #Let's try this again
+@dashboard_api.route('/mandrillreplies', methods=['POST']) #Let's try this again 457
 def save_received_user_mandrill_email():
     """
     print "save_received_user_mandrill_email"
